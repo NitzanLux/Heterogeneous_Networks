6deab353ab9372fa52349f1dd7e56157c6bf5d41
goolux <30436343+goolux@users.noreply.github.com>
2023-04-27 17:05:40 +0300

    artificial_ann

---
+++ utils/clear_by_name_script.py
@@ -0,0 +1,23 @@
+import time
+import os
+import logging
+import argparse
+import json
+from utils.slurm_job import *
+import subprocess
+import re
+from utils.cluster_aid_methods import get_works_on_cluster
+parser = argparse.ArgumentParser(description='json file...')
+
+parser.add_argument('-re',dest="job_name_format", type=str,
+                    help='configurations json file of paths')
+
+args = parser.parse_args()
+deleted_names = get_works_on_cluster(f"{args.job_name_format}")
+delete_all = input('delete all those files? (y/n)?')
+# delete_all = subprocess.run(["read answer"], stdout=subprocess.PIPE)
+if delete_all=='y':
+    for i in deleted_names:
+        command = ['scancel',f'--jobname={i}']
+        # print(command)
+        subprocess.run(command, stdout=subprocess.PIPE)
