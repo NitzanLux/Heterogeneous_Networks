3dab26b824d39e8b6bfd96f8261b285e8aafe97a
goolux <30436343+goolux@users.noreply.github.com>
2023-04-27 15:35:35 +0300

    artificial_ann

--- utils/pack_to_one_dict.py
+++ utils/pack_to_one_dict.py
@@ -9,13 +9,23 @@ for base_folder in os.listdir('data'):
     cur_path=os.path.join('data',base_folder)
     data=None
     for i in os.listdir(cur_path):
+        if i=='summaries':
+            continue
         with open(os.path.join(cur_path, i), 'rb') as fp:
             cur_data = pickle.load(fp)
         if data is None:
             data = cur_data
         else:
-            for k,v in cur_data.items():
-                data[k].append(v)
+            length=None
+            for k in cur_data.keys():
+                if length is None:
+                    length = len(cur_data[k])
+                if length!=len(cur_data[k]):
+                    break
+            else:
+                for k,v in cur_data.items():
+                    data[k].append(v)
+
     dest_path=os.path.join('data','summaries')
     os.makedirs(dest_path,exist_ok=True)
     with open(os.path.join(dest_path,f'{base_folder}.p'), 'wb') as fp:
