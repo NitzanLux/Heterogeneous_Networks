05e0a07a678e0b8684b8f082e99d9eaf62a20960
goolux <30436343+goolux@users.noreply.github.com>
2023-04-27 15:58:06 +0300

    artificial_ann

--- utils/pack_to_one_dict.py
+++ utils/pack_to_one_dict.py
@@ -5,32 +5,36 @@ try:
 except ImportError:  # Python 3.x
     import pickle
 # os.makedirs(os.path.join('data','summaries'), exist_ok=True)
-for base_folder in os.listdir('data'):
-    cur_path=os.path.join('data',base_folder)
-    data=None
+summaries_flag = True
+for base_folder in os.listdir('data') + ['summaries']:
+    if base_folder == 'summaries' and summaries_flag:
+        summaries_flag=False
+        continue
+    cur_path = os.path.join('data', base_folder)
+    data = None
     for i in os.listdir(cur_path):
-        if i=='summaries':
+        if i == 'summaries':
             continue
         with open(os.path.join(cur_path, i), 'rb') as fp:
             cur_data = pickle.load(fp)
         if data is None:
             data = cur_data
         else:
-            length=None
+            length = None
             for k in cur_data.keys():
-                if k=='condition': continue
+                if k == 'condition': continue
                 if length is None:
                     length = len(cur_data[k])
-                if length==0:
+                if length == 0:
                     break
-                if length!=len(cur_data[k]):
-                    print("\n".join(["%s - %d"%(i,len(cur_data[k])) for i in cur_data.keys()]))
+                if length != len(cur_data[k]):
+                    print("\n".join(["%s - %d" % (i, len(cur_data[k])) for i in cur_data.keys()]))
                     break
             else:
-                for k,v in cur_data.items():
+                for k, v in cur_data.items():
                     data[k].extend(v)
 
-    dest_path=os.path.join('data','summaries')
-    os.makedirs(dest_path,exist_ok=True)
-    with open(os.path.join(dest_path,f'{base_folder}.p'), 'wb') as fp:
+    dest_path = os.path.join('data', 'summaries')
+    os.makedirs(dest_path, exist_ok=True)
+    with open(os.path.join(dest_path, f'{base_folder}.p'), 'wb') as fp:
         pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
