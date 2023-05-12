20a7104173889c6fee6b0b8ca9ef3864fc0ba24b
goolux <30436343+goolux@users.noreply.github.com>
2023-05-12 15:46:52 +0300

    appdate_costume lr

--- permuted_mnist_main.py
+++ permuted_mnist_main.py
@@ -148,8 +148,8 @@ if __name__ == '__main__':
             s = slurm_job.SlurmJobFactory('cluster_logs')
             args['homogeneous_lr']=False
 
-            s.send_job_for_function(f'{i}_first_validation_hetro','permuted_mnist_main','save_matrix_and_params',args,run_on_GPU=i<5)
+            s.send_job_for_function(f'{i}_first_validation_hetro','permuted_mnist_main','save_matrix_and_params',args,run_on_GPU=i<3)
             args['homogeneous_lr']=True
-            s.send_job_for_function(f'{i}_first_validation_homogenuos','permuted_mnist_main','save_matrix_and_params',args,run_on_GPU=i<5)
+            s.send_job_for_function(f'{i}_first_validation_homogenuos','permuted_mnist_main','save_matrix_and_params',args,run_on_GPU=i<3)
             print(i)
         # print(p)
