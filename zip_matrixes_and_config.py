4a87b649d94f4cd67ea2597170cec3fa3c548791
goolux <30436343+goolux@users.noreply.github.com>
2023-05-18 23:04:20 +0300

    appdate_costume lr

--- zip_matrixes_and_config.py
+++ zip_matrixes_and_config.py
@@ -7,16 +7,18 @@ def zip_matching_folders(data_folder, r_str, files_to_zip):
     for root, dirs, _ in os.walk(data_folder):
         for dir in dirs:
             if re.match(r_str, dir):
+
                 sub_folder = os.path.join(root, str(dir))
-                for _, sub_dir, _ in os.walk(sub_folder):
-                    zip_path = f"{sub_folder}/{sub_dir}.zip"
-                    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-                        sub_sub_folder = os.path.join(sub_folder, sub_dir)
-                        for _, _, files in os.walk(sub_sub_folder):
-                            for file in files:
-                                if file in files_to_zip:
-                                    abs_path = os.path.join(sub_sub_folder, file)
-                                    zipf.write(abs_path, os.path.relpath(abs_path, sub_sub_folder))
+                zip_path = f"{sub_folder}.zip"
+                with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                    for _, sub_dirs, _ in os.walk(sub_folder):
+                        for sub_dir in sub_dirs:
+                                sub_sub_folder = os.path.join(sub_folder, sub_dir)
+                                for _, _, files in os.walk(sub_sub_folder):
+                                    for file in files:
+                                        if file in files_to_zip:
+                                            abs_path = os.path.join(sub_sub_folder, file)
+                                            zipf.write(abs_path, os.path.relpath(abs_path, sub_sub_folder))
 
 parser = argparse.ArgumentParser(description='Process some strings.')
 parser.add_argument('r_str', type=str, help='The regular expression to match folder names')
